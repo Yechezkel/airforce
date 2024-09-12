f6dca25175ae76add1f99306c4bf4e5937226c48
rafi053 <rafi053313@gmail.com>
2024-09-12 19:47:18 +0300

    2 שינויים קטנים ותוקנו הבאגים שהיו לי בגירסה הראשונה הראשונה שהעליתי (הגרסה בשם 18:00)

--- scores.py
+++ scores.py
@@ -1,16 +1,16 @@
 import random
 def get_distance_score(distance):
-    magic_number = (100/distance)* 150;
+    magic_number = (100/distance)* 150
     if magic_number > 150:
-        return 150;
+        return 150
     if magic_number < 1:
         return 1
     return magic_number
 
 def get_aircraft_fuel_score(aircraft_fuel_Capacity):
-    magic_number = (4000/aircraft_fuel_Capacity)*200;
+    magic_number = (4000/aircraft_fuel_Capacity)*200
     if magic_number > 200:
-        return 200;
+        return 200
     if magic_number < 1:
         return 1
     return magic_number
@@ -25,7 +25,7 @@ def get_pilot_skill_score(pilot_skill):
 
 
 def get_weather_conditions_score(weather_conditions: int):
-    return weather_conditions * 200
+    return int(weather_conditions) * 200
 
 
 def get_priority_score(priority: int):
